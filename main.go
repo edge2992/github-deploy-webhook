33288730b8f1076572b30e0033ad6bc5d42f6cb5
edge2992 <torune.summer@gmail.com>
2023-12-10 16:30:56 +0900

    deploy script

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"log"
@@ -8,15 +10,31 @@ import (
 	"os/exec"
 )
 
+var secret = []byte("adfgadfe123FDSa")
+
+func varifySignature(message, providedSignature []byte) bool {
+	mac := hmac.New(sha256.New, secret)
+	mac.Write(message)
+	expectedMAC := mac.Sum(nil)
+	expectedSignature := fmt.Sprintf("sha256=%x", expectedMAC)
+	return hmac.Equal([]byte(expectedSignature), providedSignature)
+}
+
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
 		return
 	}
 
+	signature := r.Header.Get("X-Hub-Signature-256")
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Error reading body", http.StatusInternalServerError)
+		return
+	}
+
+	if !varifySignature(body, []byte(signature)) {
+		http.Error(w, "Invalid signature", http.StatusForbidden)
 		return
 	}
 
@@ -26,9 +44,12 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	err = cmd.Run()
 	if err != nil {
 		log.Printf("Deployment script failed: %s", err)
+		http.Error(w, "Deployment script failed", http.StatusInternalServerError)
 		return
 	}
 	log.Println("Deployment script succeeded")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Deployment script succeeded"))
 	// 応答を返す
 }
 
